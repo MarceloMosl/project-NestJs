6171965c386a51d5ab9255945b33f9da5665a45e
MarceloMosl <marcelomosl@icloud.com>
2023-07-16 18:48:08 -0300

    refact: error when title exists on create publication

--- src/modules/publications/publications.service.ts
+++ src/modules/publications/publications.service.ts
@@ -8,7 +8,7 @@ export class PublicationsService {
   constructor(private readonly postsRepo: PublicationsRepo) {}
   async create(body: postDto, user: User) {
     const titleExists = await this.postsRepo.getByTitle(body.title);
-    if (titleExists) throw new Error('Erro na verificação do titulo');
+    if (titleExists) throw new ConflictException();
 
     console.log({ body, user });
     return await this.postsRepo.create(body, user.id);
